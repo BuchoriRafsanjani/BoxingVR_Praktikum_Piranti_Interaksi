3e5d3fec0f4ab101bcad479a8115412042c70092
Buchori Rafsanjani <chorydream@gmail.com>
2018-05-29 22:04:54 +0700

    Progress Game Gear VR with Controller (Game Boxing VR)
    Done :
    1. Terdapat start menu
    2. Scoring dan high score sudah ada
    3. perbaikan support gear vr
    4. terdapat kondisi game over
    5. konsep game dimodifikasi menjadi meninju monster (dengan menggunakan gear vr controller) yang ada supaya projeknya sama dengan yang ada pada praktikum multiplayer
    
    Next Update :
    1. Perbaikan bug pada collision
    2. finishing asset
    3. penyempurnaan gameplay

--- Boxing VR Using Gear VR/Assets/Scripts/LaserControl.cs
+++ Boxing VR Using Gear VR/Assets/Scripts/LaserControl.cs
@@ -2,18 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserControl : MonoBehaviour {
+public class LaserControl : MonoBehaviour
+{
 
-    //public GameObject LeftLaser;
-    //public GameObject RightLaser;
+    private LineRenderer laser;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // posisi laser yang dekat dengan camera
+        laser.SetPosition(0, transform.position);
+
+        // mengecek object yang terdapat didepan camera
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            // mengecek object yang dideteksi harus memiliki collider
+            if (hit.collider)
+            {
+                //jarak ujung laser berdasarkan posisi laser terkena object
+                laser.SetPosition(1, hit.point);
+              
+                // pastikan object yang dideteksi memiliki tag UI
+                if (hit.transform.tag.Equals("UI"))
+                {
+                    hit.transform.GetComponent<RespondButton>().SetHit();
+                    
+                    // ketika user menekan/klik tombol
+                    if (OVRInput.GetDown(OVRInput.RawButton.A))
+                    {
+                        hit.transform.GetComponent<RespondButton>().SetClick();
+                    }
+                }
+            }
+        }
+        else
+        {
+            // memberi batasan jarak laser terjauh dengan nilai maksimal 5000
+            laser.SetPosition(1, transform.forward * 5000);
+        }
+    }
 }
